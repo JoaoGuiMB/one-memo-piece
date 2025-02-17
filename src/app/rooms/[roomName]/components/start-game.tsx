8920ec685b9254b29f4741dcc69241bc9d46c9d0
JoaoGuiMB <juaogui159@gmail.com>
2025-02-17 20:45:55 -0300

    fix: start game

--- src/app/rooms/[roomName]/components/start-game.tsx
+++ src/app/rooms/[roomName]/components/start-game.tsx
@@ -3,12 +3,15 @@
 import { Button } from "~/components/ui/button";
 import { startGame } from "~/app/actions/room";
 import { useMutation } from "@liveblocks/react";
-import { LiveList } from "@liveblocks/client";
+import { LiveList, LiveObject } from "@liveblocks/client";
 
 export default function StartGameButton({ roomName }: { roomName: string }) {
   const handleStartGame = useMutation(async ({ storage, self }) => {
     const generatedCards = (await startGame(roomName)) ?? [];
-    storage.set("gameCards", new LiveList(generatedCards));
+    storage.set(
+      "gameCards",
+      new LiveList(generatedCards.map((card) => new LiveObject(card))),
+    );
   }, []);
 
   return <Button onClick={() => handleStartGame()}>Start Game</Button>;
